6453a15fd9322bed3340dca0ff9bf3490c5e2f28
Sheetal <onlysheetal@gmail.com>
2012-09-04 19:41:43 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samplenewest::Application.config.secret_token = '4c3f3a447e0c7505007d281cac46f82452f8e771916486cf24c031a38d6f43caa0490769529d976fc1dd5cd3adf450f18152e27ffa852e0849ea5ef432e19eea'
